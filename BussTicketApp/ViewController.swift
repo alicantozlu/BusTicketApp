97364e0c5ed844e5d764c0049ddd9684e906e053
Ali Can Tozlu <alicantozlu@gmail.com>
2022-02-11 18:04:07 +0300

    Initial Commit

---
+++ BussTicketApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BussTicketApp
+//
+//  Created by Ali Can Tozlu on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
